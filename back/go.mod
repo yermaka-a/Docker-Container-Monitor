330fcc9e313bbb4b9075ac423363f22f778eb95b
yermaka <ermak.080@mail.ru>
2025-02-07 04:16:05 +0800

    feat: confiugre docker; add .env

---
+++ back/go.mod
@@ -0,0 +1,3 @@
+module back/api
+
+go 1.23.4
--- back/main.go
+++ back/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// const (
+// 	host     = "localhost"
+// 	port     = 5432
+// 	user     = "yourusername"
+// 	password = "yourpassword"
+// 	dbname   = "yourdbname"
+// )
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World!")
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
