cd7625c2595eaea08a10b1f1fe748e3f57a8f58e
yermaka <ermak.080@mail.ru>
2025-02-08 18:22:49 +0800

    back folders structure modified

---
+++ back/internal/app/app.go
@@ -0,0 +1,36 @@
+package app
+
+import (
+	"back/internal/config"
+	"back/internal/requests"
+	"fmt"
+	"net/http"
+
+	"github.com/rs/cors"
+)
+
+const (
+	PORT = ":3010"
+)
+
+func Start() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/containers", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			requests.GetContainers(w, r)
+		case http.MethodPost:
+			requests.UpdateContainers(w, r)
+		default:
+			http.Error(w, "Unsupported request method", http.StatusMethodNotAllowed)
+		}
+	})
+	FRONT_PORT := config.New().FrontConfig.FRONT_PORT
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://pinger", "http://front", fmt.Sprintf("http://localhost:%s", FRONT_PORT)}, // Разрешенные домены
+		AllowedMethods:   []string{"GET", "POST", "OPTIONS"},
+		AllowedHeaders:   []string{"Content-Type"},
+		AllowCredentials: true,
+	})
+	http.ListenAndServe(PORT, c.Handler(mux))
+}
---
+++ back/internal/requests/httpRequests.go
@@ -0,0 +1,29 @@
+package requests
+
+import (
+	"back/internal/db"
+	"encoding/json"
+	"net/http"
+)
+
+func GetContainers(w http.ResponseWriter, r *http.Request) {
+	containers := db.GetContainers()
+	w.Header().Set("Content-type", "application/json")
+	if err := json.NewEncoder(w).Encode(containers); err != nil {
+		http.Error(w, "Error encoding JSON", http.StatusInternalServerError)
+		return
+	}
+}
+func UpdateContainers(w http.ResponseWriter, r *http.Request) {
+	var containers []db.Container
+	if err := json.NewDecoder(r.Body).Decode(&containers); err != nil {
+		http.Error(w, "Invalid input!", http.StatusBadRequest)
+		return
+	}
+	result := db.UpdateContainers(&containers)
+	w.Header().Set("Content-type", "application/json")
+	if err := json.NewEncoder(w).Encode(result); err != nil {
+		http.Error(w, "Error encoding JSON", http.StatusInternalServerError)
+		return
+	}
+}
